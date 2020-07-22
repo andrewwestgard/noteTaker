ad582e1f5bcf95b0559f06f30002c10eb7dd5a47
andrewwestgard <westgaar9629@uwec.edu>
2020-07-21 20:19:04 -0500

    added JAWS_db for deployment purposes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 const mysql = require('mysql');
 const util = require('util');
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -8,7 +11,7 @@ const connection = mysql.createConnection({
     password: '',
     database: 'notes_db'
   });
-
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -16,6 +19,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
 // we give connection.query access to promises
 // i.e. .then() and .catch()
 connection.query = util.promisify(connection.query);
